073d39be6310b8dc7a15b3a3ca862bb53347cd8a
lblue <2319406132@qq.com>
2019-03-20 17:56:24 +0800

    use etcd3

--- store/etcd_demo.py
+++ store/etcd_demo.py
@@ -1,8 +1,33 @@
-from etcd import Client
+import etcd3
+import time
+import _thread as thread
 
-client = Client(host="localhost", port=2379)
-client.write("/demo/demo", 1)
-print(client.read("/demo"))
-for e in client.eternal_watch("/demo", recursive=True):
-    print(e.value)
+
+client = etcd3.client(host="localhost", port=2379)
+client.put("/demo/demo", "1")
+print(list(client.get_prefix("/demo")))
+
+
+def watch_callback(event):
+    print("update etcd")
+    print(event)
+
+
+# watch_id = client.add_watch_callback("/demo", watch_callback)
+def watch_event(*args, **kwargs):
+    event_iter, cance = client.watch_prefix("/demo/")
+    for e in event_iter:
+        print("updating etcd")
+        print(e)
+
+
+thread.start_new_thread(watch_event, (None, None))
+
+# i = 0
+# while True:
+#     # print(1)
+#     # i += 1
+#     time.sleep(1)
+
+
 
--- store/etcd_write.py
+++ store/etcd_write.py
@@ -1,8 +1,9 @@
 import time
-from etcd import Client
+import etcd3
 
-client = Client(host="localhost", port=2379)
+
+client = etcd3.client(host="localhost", port=2379)
 t = time.time()
-client.delete("/demo", recursive=True)
-# client.write("/demo/%s" % int(t), t)
+# client.delete("/demo", recursive=True)
+client.put("/demo/%s" % int(t), str(t))
 # print(client.read("/demo"))
